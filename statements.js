6b196f2b52e979b848fe70eedf3012b30de9b9a9
mirror <mironosk@gmail.com>
2016-05-06 11:42:59 +0300

    finish

--- statements.js
+++ statements.js
@@ -1,11 +1,46 @@
 'use strict'
 
+
+
+var str1 = '123x1z13', str2 = 'a123';
+
+console.log(sum(str1,str2));
+
+function sum() {
+	var toSum = [];
+	var sumNumbers = 0; 
+	for (var i = 0; i < arguments.length; i++) {
+		parseDigit(arguments[i]);
+	}
+
+	for(var j = 0; j < toSum.length; j++){
+		sumNumbers += toSum[j];
+	}
+
+	return sumNumbers;
+
+	function parseDigit(str){
+		var patt =/[^1-9ABCDEF]/ig;
+		var pattIsHex = /[ABCDEF]/i;
+		str = str.replace(patt, '');
+		var number;
+		if (pattIsHex.test(str)) {
+			number= parseInt(str, 16)
+		}else{
+			number =parseInt(str, 10)
+		}
+		if (!isNaN(number)) {
+			toSum[i] = number;
+		}
+	}
+}
+
+
 var data = ";key,value;key1,value;key3,value3;";
 parser(data);
 
 var data = ";key,value;key1,value;arrayHere:k1,v1;k2,v2;k3,v3";
 parser(data);
-
 
 
 
